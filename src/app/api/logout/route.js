d3bf32bbf2c184b19e649d8f368de9b01c791aed
Fernando Ituarte <fernando.ituarte@oclock.school>
2024-11-10 15:28:05 +0100

    Implement cookie deletion in logout route

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -1,22 +1,25 @@
 import { cookies } from "next/headers";
 
 export async function DELETE(req) {
-  const cookie = await cookies();
-  if (req.method === "DELETE") {
-    cookie.delete({
-      name: "authToken",
-    });
-    cookie.delete({
-      name: "user",
-    });
+  try {
+    const cookieStore = await cookies();
+
+    cookieStore.set("authToken", "", { maxAge: -1 });
+    cookieStore.set("user", "", { maxAge: -1 });
 
     return new Response(
       JSON.stringify({
         success: true,
         status: 201,
       }),
+      {
+        status: 201,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
     );
-  } else {
+  } catch (error) {
     return new Response(
       JSON.stringify({ error: `Method ${req.method} Not Allowed` }),
       {
